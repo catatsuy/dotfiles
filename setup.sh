6a19ebac6c159ec1a3c97957ab34cc83f80b4665
catatsuy <catatsuy@catatsuy.org>
2013-10-13 18:46:48 +0900

    setup.sh for rbenv plenv

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# rbenv
+git clone https://github.com/sstephenson/rbenv.git $HOME/.rbenv
+mkdir -p $HOME/.rbenv/plugins
+git clone https://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+git clone https://github.com/sstephenson/rbenv-default-gems.git $HOME/.rbenv/plugins/rbenv-default-gems
+
+# plenv
+git clone https://github.com/tokuhirom/plenv.git $HOME/.plenv
+mkdir -p $HOME/.plenv/plugins
+git clone https://github.com/tokuhirom/Perl-Build.git $HOME/.plenv/plugins/perl-build
