b24a64f8554c0ff087bc54ed17fbc969f892e5cf
catatsuy <catatsuy@catatsuy.org>
2013-10-13 19:21:03 +0900

    homebrew package

---
+++ setup_Darwin.sh
@@ -0,0 +1,18 @@
+brew install --disable-etcdir zsh
+brew install tmux
+brew install reattach-to-user-namespace
+brew install git
+brew install coreutils
+brew install mobile-shell
+brew install nkf
+brew install colordiff
+brew install wget
+brew install tig
+brew install --with-lua --with-perl --override-system-vi vim
+brew install scala
+brew install haskell-platform
+
+# emacs
+brew install cmigemo
+brew install --with-lang-en aspell
+
