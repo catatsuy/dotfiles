ac2558d8acb2ba000d2648d6529ebe26085e3b05
catatsuy <catatsuy@catatsuy.org>
2014-02-19 15:22:31 +0900

    add extra package

--- debian.sh
+++ debian.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-sudo apt-get install -y debconf-utils less lsof lv nkf openssl sudo rsync unzip dstat hdparm linux-headers-`uname -r` tree curl
+# common tools
+sudo apt-get install -y debconf-utils less lsof lv ncurses-term nkf ntpdate openssl openssh-server sudo rsync time unzip dstat iptstate hdparm linux-headers-`uname -r` tree
 
 sudo apt-get install -y autoconf automake libtool build-essential pkg-config subversion git git-svn tig vim-nox screen zsh tmux
+
+# network tools
+sudo apt-get install -y bind9-host curl dnsutils ethtool iftop ngrep tcpdump telnet whois
 
 update-alternatives --set editor /usr/bin/vim.nox
 
