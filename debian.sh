09521248ddb0b73045253c57c3b6501040329ace
catatsuy <catatsuy@catatsuy.org>
2015-02-12 13:26:07 +0900

    Ruby2.2移行に必要

--- debian.sh
+++ debian.sh
@@ -18,3 +18,4 @@ aptitude install -y ntp
 aptitude install -y libssl-dev
 aptitude install -y libreadline5 libreadline-gplv2-dev
 aptitude install -y zlib1g zlib1g-dev
+aptitude install -y libffi-dev
