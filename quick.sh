7f3598b4157b91e3832601978fa7950154a13ced
catatsuy <catatsuy@catatsuy.org>
2015-09-19 20:10:28 +0900

    make依存外し

--- quick.sh
+++ quick.sh
@@ -2,6 +2,6 @@
 
 git clone --recursive git://github.com/catatsuy/dotfiles.git ~/dotfiles
 
-(cd ~/dotfiles && make)
+(cd ~/dotfiles && ./setup.sh)
 
 git clone --recursive git://github.com/catatsuy/dot.zsh.git ~/.zsh
---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# symbolic link
+ln -sf ~/dotfiles/.tmux.conf        ~/.tmux.conf
+ln -sf ~/dotfiles/.zshenv           ~/.zshenv
+ln -sf ~/dotfiles/.vimrc            ~/.vimrc
+ln -sf ~/dotfiles/global.gitignore  ~/.gitignore
+ln -sf ~/dotfiles/.aspell.conf      ~/.aspell.conf
+ln -sf ~/dotfiles/dircolors-solarized/dircolors.256dark  ~/.dircolors
+ln -sf ~/dotfiles/.screenrc         ~/.screenrc
+ln -sf ~/dotfiles/.emacs            ~/.emacs
+ln -sf ~/dotfiles/.gemrc            ~/.gemrc
+
+# git config
+git config --global core.editor vi
+git config --global color.ui auto
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
+git config --global merge.ff false
+git config --global core.autocrlf input
+git config --global core.excludesfile ~/.gitignore
+git config --global core.pager "less -R --tabs=4"
+git config --global ghq.root ~/src
