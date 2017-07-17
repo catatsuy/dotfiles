7c1fd7e9451479f471ded27d1ca7bfc0f9fe5033
catatsuy <catatsuy@catatsuy.org>
2017-07-17 18:14:36 +0900

    change https

--- quick.sh
+++ quick.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git clone --recursive git://github.com/catatsuy/dotfiles.git ~/dotfiles
+git clone --recursive https://github.com/catatsuy/dotfiles.git ~/dotfiles
 
 (cd ~/dotfiles && ./setup.sh)
 
-git clone --recursive git://github.com/catatsuy/dot.zsh.git ~/.zsh
+git clone --recursive https://github.com/catatsuy/dot.zsh.git ~/.zsh
