4f146c3fc7cdd7dc4025bc13de377a85f8b7c3c0
catatsuy <catatsuy@catatsuy.org>
2018-06-18 12:16:33 +0900

    commit

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ ln -sf ~/dotfiles/global.gitignore  ~/.gitignore
 ln -sf ~/dotfiles/.aspell.conf      ~/.aspell.conf
 ln -sf ~/dotfiles/dircolors-solarized/dircolors.256dark  ~/.dircolors
 ln -sf ~/dotfiles/.screenrc         ~/.screenrc
-ln -sf ~/dotfiles/.emacs            ~/.emacs
 ln -sf ~/dotfiles/.toprc            ~/.toprc
 ln -sf ~/dotfiles/.gemrc            ~/.gemrc
 
