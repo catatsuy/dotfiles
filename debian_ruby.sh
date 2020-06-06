1660416956b63dc0391fcd2efe43a1238c37f0dd
catatsuy <catatsuy@catatsuy.org>
2020-06-06 16:02:34 +0900

    remove

--- debian_ruby.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# for ruby
-aptitude install -y libssl-dev
-aptitude install -y libreadline5 libreadline-gplv2-dev
-aptitude install -y zlib1g zlib1g-dev
-aptitude install -y libffi-dev
