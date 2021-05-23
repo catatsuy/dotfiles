4d73fec84c8af976e277612066f9f4e1c27220a9
catatsuy <catatsuy@catatsuy.org>
2021-05-23 15:17:38 +0900

    use .config/git/ignore

--- setup.sh
+++ setup.sh
@@ -4,9 +4,11 @@
 ln -sf ~/dotfiles/.tmux.conf        ~/.tmux.conf
 ln -sf ~/dotfiles/.zshenv           ~/.zshenv
 ln -sf ~/dotfiles/.vimrc            ~/.vimrc
-ln -sf ~/dotfiles/global.gitignore  ~/.gitignore
 ln -sf ~/dotfiles/dircolors-solarized/dircolors.256dark  ~/.dircolors
 ln -sf ~/dotfiles/.screenrc         ~/.screenrc
+
+mkdir -p ~/.config/git/
+ln -sf ~/dotfiles/global.gitignore ~/.config/git/ignore
 
 # git config
 git config --global core.editor vi
@@ -16,6 +18,5 @@ git config --global color.status auto
 git config --global color.branch auto
 git config --global merge.ff false
 git config --global core.autocrlf input
-git config --global core.excludesfile ~/.gitignore
 git config --global core.pager "less -R --tabs=4"
 # git config --global ghq.root ~/src
